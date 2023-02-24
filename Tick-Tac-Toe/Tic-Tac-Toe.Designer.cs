d15a2d888446d8fa96e00db53c2036526f52d683
Brian Nicewander <Brian_Nicewander@hotmail.com>
2023-02-24 10:12:31 -0600

    Add project files.

---
+++ Tick-Tac-Toe/Tic-Tac-Toe.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Tick_Tac_Toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblWinner = new System.Windows.Forms.Label();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblZeroZero = new System.Windows.Forms.Label();
+            this.lblZeroOne = new System.Windows.Forms.Label();
+            this.lblZeroTwo = new System.Windows.Forms.Label();
+            this.lblOneZero = new System.Windows.Forms.Label();
+            this.lblOneOne = new System.Windows.Forms.Label();
+            this.lblOneTwo = new System.Windows.Forms.Label();
+            this.lblTwoZero = new System.Windows.Forms.Label();
+            this.lblTwoOne = new System.Windows.Forms.Label();
+            this.lblTwoTwo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblWinner
+            // 
+            this.lblWinner.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblWinner.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWinner.Location = new System.Drawing.Point(12, 304);
+            this.lblWinner.Name = "lblWinner";
+            this.lblWinner.Size = new System.Drawing.Size(321, 28);
+            this.lblWinner.TabIndex = 0;
+            this.lblWinner.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Location = new System.Drawing.Point(83, 345);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(75, 23);
+            this.btnNewGame.TabIndex = 1;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(186, 345);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblZeroZero
+            // 
+            this.lblZeroZero.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblZeroZero.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblZeroZero.Location = new System.Drawing.Point(12, 9);
+            this.lblZeroZero.Name = "lblZeroZero";
+            this.lblZeroZero.Size = new System.Drawing.Size(103, 88);
+            this.lblZeroZero.TabIndex = 3;
+            this.lblZeroZero.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblZeroOne
+            // 
+            this.lblZeroOne.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblZeroOne.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblZeroOne.Location = new System.Drawing.Point(121, 9);
+            this.lblZeroOne.Name = "lblZeroOne";
+            this.lblZeroOne.Size = new System.Drawing.Size(103, 88);
+            this.lblZeroOne.TabIndex = 4;
+            this.lblZeroOne.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblZeroTwo
+            // 
+            this.lblZeroTwo.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblZeroTwo.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblZeroTwo.Location = new System.Drawing.Point(230, 9);
+            this.lblZeroTwo.Name = "lblZeroTwo";
+            this.lblZeroTwo.Size = new System.Drawing.Size(103, 88);
+            this.lblZeroTwo.TabIndex = 5;
+            this.lblZeroTwo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblOneZero
+            // 
+            this.lblOneZero.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblOneZero.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOneZero.Location = new System.Drawing.Point(12, 106);
+            this.lblOneZero.Name = "lblOneZero";
+            this.lblOneZero.Size = new System.Drawing.Size(103, 88);
+            this.lblOneZero.TabIndex = 6;
+            this.lblOneZero.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblOneOne
+            // 
+            this.lblOneOne.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblOneOne.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOneOne.Location = new System.Drawing.Point(121, 106);
+            this.lblOneOne.Name = "lblOneOne";
+            this.lblOneOne.Size = new System.Drawing.Size(103, 88);
+            this.lblOneOne.TabIndex = 7;
+            this.lblOneOne.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblOneTwo
+            // 
+            this.lblOneTwo.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblOneTwo.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOneTwo.Location = new System.Drawing.Point(230, 106);
+            this.lblOneTwo.Name = "lblOneTwo";
+            this.lblOneTwo.Size = new System.Drawing.Size(103, 88);
+            this.lblOneTwo.TabIndex = 8;
+            this.lblOneTwo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTwoZero
+            // 
+            this.lblTwoZero.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblTwoZero.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTwoZero.Location = new System.Drawing.Point(12, 204);
+            this.lblTwoZero.Name = "lblTwoZero";
+            this.lblTwoZero.Size = new System.Drawing.Size(103, 88);
+            this.lblTwoZero.TabIndex = 9;
+            this.lblTwoZero.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTwoOne
+            // 
+            this.lblTwoOne.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblTwoOne.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTwoOne.Location = new System.Drawing.Point(121, 204);
+            this.lblTwoOne.Name = "lblTwoOne";
+            this.lblTwoOne.Size = new System.Drawing.Size(103, 88);
+            this.lblTwoOne.TabIndex = 10;
+            this.lblTwoOne.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTwoTwo
+            // 
+            this.lblTwoTwo.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblTwoTwo.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTwoTwo.Location = new System.Drawing.Point(230, 204);
+            this.lblTwoTwo.Name = "lblTwoTwo";
+            this.lblTwoTwo.Size = new System.Drawing.Size(103, 88);
+            this.lblTwoTwo.TabIndex = 11;
+            this.lblTwoTwo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 387);
+            this.Controls.Add(this.lblTwoTwo);
+            this.Controls.Add(this.lblTwoOne);
+            this.Controls.Add(this.lblTwoZero);
+            this.Controls.Add(this.lblOneTwo);
+            this.Controls.Add(this.lblOneOne);
+            this.Controls.Add(this.lblOneZero);
+            this.Controls.Add(this.lblZeroTwo);
+            this.Controls.Add(this.lblZeroOne);
+            this.Controls.Add(this.lblZeroZero);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.lblWinner);
+            this.Name = "Form1";
+            this.Text = "Tic-Tac-Toe";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblWinner;
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblZeroZero;
+        private System.Windows.Forms.Label lblZeroOne;
+        private System.Windows.Forms.Label lblZeroTwo;
+        private System.Windows.Forms.Label lblOneZero;
+        private System.Windows.Forms.Label lblOneOne;
+        private System.Windows.Forms.Label lblOneTwo;
+        private System.Windows.Forms.Label lblTwoZero;
+        private System.Windows.Forms.Label lblTwoOne;
+        private System.Windows.Forms.Label lblTwoTwo;
+    }
+}
+
---
+++ Tick-Tac-Toe/Tic-Tac-Toe.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tick_Tac_Toe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            lblWinner.Text = ""; //Clears the winner label
+            string[,] gameboard = new string[3, 3]; 
+            bool playerX = true; // puts it as player x turn first
+            int gamePlaces = 0; // keeps how many spots are filled
+
+
+            while (gamePlaces < 9) //runs while the gameplace is less then 9
+            {
+                Random rand = new Random();
+                int row = rand.Next(0, 3); //gets a random number for the row
+                int col = rand.Next(0, 3); //gets a random number for the col
+
+                if (gameboard[row, col] == null) //checks to make sure there is nothing in the label it picked
+                {
+
+                    //player X turn
+                    if (playerX == true)
+                    {
+                        gameboard[row, col] = "X"; //puts a x in whatever label it is in
+                        playerX = false; //turns player to false so it is O turn
+                        gamePlaces++; //adds one to the gameplace
+                    }
+
+                    //Player O turn
+                    else
+                    {
+                        gameboard[row, col] = "O"; //puts a O in whatever label it is in
+                        playerX = true; //turns player to true so it is X turn
+                        gamePlaces++; //adds one to the gameplace
+                    }
+
+                    System.Threading.Thread.Sleep(100);//slows down how fast the loop runs.
+                }
+
+                //This is in the while loop so it will check if there is a winner after every move
+                //Checks to see if O wins
+
+                //These check to see if O won horizontal.
+                if (gameboard[0, 0] == "O" && gameboard[0, 1] == "O" && gameboard[0, 2] == "O") //If there is an O in all these spots O win
+                {
+                    lblWinner.Text = "O wins";
+                    break; //If there is a winner this breaks out of the loop
+                }
+                else if (gameboard[1, 0] == "O" && gameboard[1, 1] == "O" && gameboard[1, 2] == "O") //If there is an O in all these spots O win
+                {
+                    lblWinner.Text = "O wins";
+                    break; //If there is a winner this breaks out of the loop
+                }
+                else if (gameboard[2, 0] == "O" && gameboard[2, 1] == "O" && gameboard[2, 2] == "O") //If there is an O in all these spots O win
+                {
+                    lblWinner.Text = "O wins";
+                    break; //If there is a winner this breaks out of the loop
+                }
+
+                //These check to see if O won vertical.
+                else if (gameboard[0, 0] == "O" && gameboard[1, 0] == "O" && gameboard[2, 0] == "O") //If there is an O in all these spots O win
+                {
+                    lblWinner.Text = "O wins";
+                    break; //If there is a winner this breaks out of the loop
+                }
+                else if (gameboard[0, 1] == "O" && gameboard[1, 1] == "O" && gameboard[2, 1] == "O") //If there is an O in all these spots O win
+                {
+                    lblWinner.Text = "O wins";
+                    break; //If there is a winner this breaks out of the loop
+                }
+                else if (gameboard[0, 2] == "O" && gameboard[1, 2] == "O" && gameboard[2, 2] == "O") //If there is an O in all these spots O win
+                {
+                    lblWinner.Text = "O wins";
+                    break; //If there is a winner this breaks out of the loop
+                }
+
+                //These check to see if O won at an \ or /.
+                else if (gameboard[0, 0] == "O" && gameboard[1, 1] == "O" && gameboard[2, 2] == "O") //If there is an O in all these spots O win
+                {
+                    lblWinner.Text = "O wins";
+                    break; //If there is a winner this breaks out of the loop
+                }
+                else if (gameboard[0, 2] == "O" && gameboard[1, 1] == "O" && gameboard[2, 0] == "O") //If there is an O in all these spots O win
+                {
+                    lblWinner.Text = "O wins";
+                    break; //If there is a winner this breaks out of the loop
+                }
+
+
+                //Checks to see if X wins
+
+                //These check to see if X won horizontal.
+                if (gameboard[0, 0] == "X" && gameboard[0, 1] == "X" && gameboard[0, 2] == "X") //If there is an X in all these spots X win
+                {
+                    lblWinner.Text = "X wins";
+                    break; //If there is a winner this breaks out of the loop
+                }
+                else if (gameboard[1, 0] == "X" && gameboard[1, 1] == "X" && gameboard[1, 2] == "X") //If there is an X in all these spots X win
+                {
+                    lblWinner.Text = "X wins";
+                    break; //If there is a winner this breaks out of the loop
+                }
+                else if (gameboard[2, 0] == "X" && gameboard[2, 1] == "X" && gameboard[2, 2] == "X") //If there is an X in all these spots X win
+                {
+                    lblWinner.Text = "X wins";
+                    break; //If there is a winner this breaks out of the loop
+                }
+
+                //These check to see if X won vertical.
+                else if (gameboard[0, 0] == "X" && gameboard[1, 0] == "X" && gameboard[2, 0] == "X") //If there is an X in all these spots X win
+                {
+                    lblWinner.Text = "X wins";
+                    break; //If there is a winner this breaks out of the loop
+                }
+                else if (gameboard[0, 1] == "X" && gameboard[1, 1] == "X" && gameboard[2, 1] == "X") //If there is an X in all these spots X win
+                {
+                    lblWinner.Text = "X wins";
+                    break; //If there is a winner this breaks out of the loop
+                }
+                else if (gameboard[0, 2] == "X" && gameboard[1, 2] == "X" && gameboard[2, 2] == "X") //If there is an X in all these spots X win
+                {
+                    lblWinner.Text = "X wins";
+                    break; //If there is a winner this breaks out of the loop
+                }
+
+                //These check to see if X won at an \ or /.
+                else if (gameboard[0, 0] == "X" && gameboard[1, 1] == "X" && gameboard[2, 2] == "X") //If there is an X in all these spots X win
+                {
+                    lblWinner.Text = "X wins";
+                    break; //If there is a winner this breaks out of the loop
+                }
+                else if (gameboard[0, 2] == "X" && gameboard[1, 1] == "X" && gameboard[2, 0] == "X") //If there is an X in all these spots X win
+                {
+                    lblWinner.Text = "X wins";
+                    break; //If there is a winner this breaks out of the loop
+                }
+
+                if (gamePlaces == 9)//This runs if there is no winner after all 9 spots are filled up and no one has won
+                {
+                    lblWinner.Text = "It is a tie";
+                }
+            }
+
+            //This prints the results from the loop above
+
+            if (gameboard[0, 0] != null) //makes sure the label has something in it then puts whatever it is in it.
+            {
+                lblZeroZero.Text = gameboard[0, 0].ToString();
+            }
+            else //if there is nothing in the label it puts a empty space in it.
+            {
+                lblZeroZero.Text = " "; 
+            }
+
+            if (gameboard[0, 1] != null) //makes sure the label has something in it then puts whatever it is in it.
+            {
+                lblZeroOne.Text = gameboard[0, 1].ToString();
+            }
+            else //if there is nothing in the label it puts a empty space in it.
+            {
+                lblZeroOne.Text = " "; 
+            }
+
+            if (gameboard[0, 2] != null) //makes sure the label has something in it then puts whatever it is in it.
+            {
+                lblZeroTwo.Text = gameboard[0, 2].ToString();
+            }
+            else //if there is nothing in the label it puts a empty space in it.
+            {
+                lblZeroTwo.Text = " "; 
+            }
+
+            if (gameboard[1, 0] != null) //makes sure the label has something in it then puts whatever it is in it.
+            {
+                lblOneZero.Text = gameboard[1, 0].ToString();
+            }
+            else //if there is nothing in the label it puts a empty space in it.
+            {
+                lblOneZero.Text = " ";
+            }
+
+            if (gameboard[1, 1] != null) //makes sure the label has something in it then puts whatever it is in it.
+            {
+                lblOneOne.Text = gameboard[1, 1].ToString();
+            }
+            else //if there is nothing in the label it puts a empty space in it.
+            {
+                lblOneOne.Text = " ";
+            }
+
+            if (gameboard[1, 2] != null) //makes sure the label has something in it then puts whatever it is in it.
+            {
+                lblOneTwo.Text = gameboard[1, 2].ToString();
+            }
+            else //if there is nothing in the label it puts a empty space in it.
+            {
+                lblOneTwo.Text = " ";
+            }
+
+            if (gameboard[2, 0] != null) //makes sure the label has something in it then puts whatever it is in it.
+            {
+                lblTwoZero.Text = gameboard[2, 0].ToString();
+            }
+            else //if there is nothing in the label it puts a empty space in it.
+            {
+                lblTwoZero.Text = " ";
+            }
+
+            if (gameboard[2, 1] != null) //makes sure the label has something in it then puts whatever it is in it.
+            {
+                lblTwoOne.Text = gameboard[2, 1].ToString();
+            }
+            else //if there is nothing in the label it puts a empty space in it.
+            {
+                lblTwoOne.Text = " ";
+            }
+
+            if (gameboard[2, 2] != null) //makes sure the label has something in it then puts whatever it is in it.
+            {
+                lblTwoTwo.Text = gameboard[2, 2].ToString();
+            }
+            else //if there is nothing in the label it puts a empty space in it.
+            {
+                lblTwoTwo.Text = " ";
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {//This closes the program
+            this.Close();
+        }
+    }
+}
